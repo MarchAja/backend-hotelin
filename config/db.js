c3da162b8edd8eb290e49e4983d3f31c0fcaf9b2
Marcellino David Kumaseh <marcellino.10123263@mahasiswa.unikom.ac.id>
2025-12-06 15:19:12 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST,         // contoh: auth-db1158.hstgr.io
-  user: process.env.DB_USER,         // contoh: u663413022_hotelin
+  host: process.env.DB_HOST,      // contoh: auth-db1158.hstgr.io
+  user: process.env.DB_USER,      // contoh: u663413022_hotelin
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
 });
 
-// TEST KONEKSI (boleh dihapus nanti setelah yakin)
+// TEST KONEKSI (tidak akan bikin crash, cuma log hasilnya)
 (async () => {
   try {
     const conn = await pool.getConnection();
